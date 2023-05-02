569c345deefbdc7a55a5501d78abe23bb77688d6
Jeff Hykin <jeff.hykin@gmail.com>
2023-05-02 11:12:56 -0500

    formatting

--- src/graph.hpp
+++ src/graph.hpp
@@ -8,88 +8,87 @@
  */
 
 #ifndef __GRAPH__
-#define __GRAPH__
+    #define __GRAPH__
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <vector>
-#include <assert.h>
-#include "fileio.h"
-using namespace std;
+    #include <stdio.h>
+    #include <stdlib.h>
+    #include <vector>
+    #include <assert.h>
+    #include "fileio.h"
+    using namespace std;
 
-class Graph
-{
-public:
-
-    // Null label.
-    enum { NULL_LABEL=0xffffffff };
-
-    class Edge;
-
-    // Graph vertex.
-    class Vertex
+    class Graph
     {
-public:
-        unsigned int   id;
-        unsigned int   label;
-        vector<Edge *> edges;
-        Vertex(unsigned int label = NULL_LABEL);
-        void sourceEdges(vector<Edge *>& edgeList);
+        public:
+
+            // Null label.
+            enum { NULL_LABEL=0xffffffff };
+
+            class Edge;
+
+            // Graph vertex.
+            class Vertex
+            {
+                public:
+                    unsigned int   id;
+                    unsigned int   label;
+                    vector<Edge *> edges;
+                    Vertex(unsigned int label = NULL_LABEL);
+                    void sourceEdges(vector<Edge *>& edgeList);
+            };
+
+            // Graph edge.
+            class Edge
+            {
+                public:
+                    unsigned int label;
+                    Vertex       *source;
+                    Vertex       *target;
+                    bool         directed;
+                    Edge(unsigned int label = NULL_LABEL);
+            };
+
+            // Vertices.
+            vector<Vertex *> vertices;
+
+            // Constructor.
+            Graph();
+
+            // Destructor.
+            ~Graph();
+
+            // Add vertex.
+            Vertex *addVertex(unsigned int label = NULL_LABEL);
+
+            // Connect vertices.
+            Edge *connectVertices(Vertex *source, Vertex *target, bool directed, unsigned int label = NULL_LABEL);
+
+            // Get vertex by label: returns first found.
+            Vertex *getVertex(unsigned int label);
+
+            // Clear.
+            void clear();
+
+            // Clone.
+            Graph *clone();
+
+            // Remove labeled edges by transferring labels to extra vertices.
+            Graph *labeledEdges2Vertices();
+
+            // Identify vertices.
+            void id();
+
+            // Load and save.
+            void load(FILE *fp);
+            void save(FILE *fp);
+
+            // Print.
+            void print(FILE *fp = stdout);
+            void print(char *label, FILE *fp = stdout);
+
+            // Dump graph in Graphviz "dot" format.
+            void dump(FILE *fp = stdout);
+            void dump(char *label, FILE *fp = stdout);
+            void dumpSub(FILE *fp = stdout);
     };
-
-    // Graph edge.
-    class Edge
-    {
-public:
-        unsigned int label;
-        Vertex       *source;
-        Vertex       *target;
-        bool         directed;
-        Edge(unsigned int label = NULL_LABEL);
-    };
-
-    // Vertices.
-    vector<Vertex *> vertices;
-
-    // Constructor.
-    Graph();
-
-    // Destructor.
-    ~Graph();
-
-    // Add vertex.
-    Vertex *addVertex(unsigned int label = NULL_LABEL);
-
-    // Connect vertices.
-    Edge *connectVertices(Vertex *source, Vertex *target,
-                                 bool directed, unsigned int label = NULL_LABEL);
-
-    // Get vertex by label: returns first found.
-    Vertex *getVertex(unsigned int label);
-
-    // Clear.
-    void clear();
-
-    // Clone.
-    Graph *clone();
-
-    // Remove labeled edges by transferring labels to extra vertices.
-    Graph *labeledEdges2Vertices();
-
-    // Identify vertices.
-    void id();
-
-    // Load and save.
-    void load(FILE *fp);
-    void save(FILE *fp);
-
-    // Print.
-    void print(FILE *fp = stdout);
-    void print(char *label, FILE *fp = stdout);
-
-    // Dump graph in Graphviz "dot" format.
-    void dump(FILE *fp = stdout);
-    void dump(char *label, FILE *fp = stdout);
-    void dumpSub(FILE *fp = stdout);
-};
 #endif
